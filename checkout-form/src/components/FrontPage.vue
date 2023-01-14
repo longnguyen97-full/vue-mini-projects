60e331187e7a38a463795862c783b6890da26c9b
liam <nhblong1234@gmail.com>
2023-01-14 21:42:39 +0700

    dev form

---
+++ checkout-form/src/components/FrontPage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Form from '../components/Form.vue'
+</script>
+
+<template>
+    <div class="front-page">
+        <button type="button" class="btn btn-sm" :class="$store.state.formButton"
+            @click="$store.commit('toggleForm', $store.state.formOpen)">Checkout <i class="fa"
+                :class="$store.state.formIcon" aria-hidden="true"></i></button>
+        <div class="container">
+            <div class="py-5 text-center">
+                <img class="d-block mx-auto mb-4" src="@/assets/logo.svg" alt="" width="72" height="72">
+                <h2>Vietnam Culture Restaurant</h2>
+                <p class="lead">{{ $store.state.formMsg }}</p>
+            </div>
+            <Form v-if="$store.state.formOpen" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.front-page {
+    margin: 5px;
+}
+</style>
--- checkout-form/src/main.js
+++ checkout-form/src/main.js
@@ -1,6 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
+import { createStore } from "vuex";
 
-createApp(App).mount('#app')
+// Create a new store instance or import from module.
+const store = createStore({
+    /* state, actions, mutations */
+    state: {
+        formOpen: false,
+        formdata: [],
+        formButton: 'btn-dark',
+        formIcon: 'fa-shopping-cart',
+        formMsg: 'Click checkout button to enter form',
+    },
+    mutations: {
+        toggleForm(state) {
+            state.formOpen = !state.formOpen;
+            state.formButton = !state.formOpen ? 'btn-dark' : 'btn-primary';
+            state.formIcon = !state.formOpen ? 'fa-shopping-cart' : 'fa-chevron-left';
+            state.formMsg = !state.formOpen ? 'Click checkout button to enter form' : 'This is a checkout form for Food Delivery';
+        },
+        add(state, data) {
+            state.formdata.push(data);
+        },
+    }
+});
+
+createApp(App).use(store).mount('#app')
